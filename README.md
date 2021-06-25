fea6c99b895a6b438ba063e52e29adf5c39cc4ec
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-06-25 16:23:07 -0700

    added comments and sqlfile

--- README.md
+++ README.md
@@ -6,9 +6,18 @@ For this assignment the goal is to build a content managing system for a company
 ## User Story
 A business owner wants to be able to view and manage their departments, roles, and employees so that they can organize and plan accordingly.
 
-## Directions 
 
 ## Acceptance Criteria
+When running index.js file with node and choices appear for user
+Given when user can add departments
+Given when user can add roles 
+Given when user can add employees
+Given when user can view department table
+Given when user can view role table
+Given when user can view employee table
+Given when user can update employee roles
+
+
 
 ## Mock up
-![Here is what my generated page looks like](./)
+![Here is what my application runs like](./)
--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,20 @@
+-- Drops the employee_db if it already exists --
 DROP DATABASE IF EXISTS employee_db;
+
+-- Creates a database called employee_db --
 CREATE DATABASE employee_db;
+
+-- Uses newly created database --
 USE employee_db;
 
+-- Create a table called departments that includes these rows --
 CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
+-- Create a table called roles that includes these rows --
 CREATE TABLE roles (
     id INT(10) NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
@@ -16,6 +23,7 @@ CREATE TABLE roles (
     PRIMARY KEY (id)
 );
 
+-- Create a table called employees that includes these rows--
 CREATE TABLE employees (
     id INT(10) NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
@@ -24,3 +32,15 @@ CREATE TABLE employees (
     manager_id INT(10),
     PRIMARY KEY (id)
 );
+
+-- Creates new rows--
+
+INSERT INTO departments (name)
+VALUES ( "x");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ( "x", "20000", "1");
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ( "nancy", "gonz", "1", "7");
+
