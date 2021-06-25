c35edb44e96e1ceefb67d15e67da87c4cfded18b
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-06-24 18:26:21 -0700

    added more list opt

--- index.js
+++ index.js
@@ -13,16 +13,24 @@ const employeeOpt = () => {
     .prompt({
         name: 'listoption',
         type: 'list',
-        choices: [ 'Add department', 'View department', 'Update employee role']//add exit for user?
+        choices: [ 'Add department', 'Add role', 'Add employee', 'View Departments', 'View Roles', 'View Employees', 'Update Employee Role']//add exit for user?
     }) 
     .then((answer) => {
         switch (answer.listoption){
             case 'Add department':
                 return addDept();
-            case 'View department':
-                return viewDept();
-            case 'Update employee role':
-                return employeeUpdate();
+            case 'Add role':
+                return addRole();
+            case 'Add employee':
+                return addEmp();
+            case 'View Departments':
+                return viewDep();
+            case 'View Roles':
+                return viewRoles();
+            case 'View Employees':
+                return viewEmp();
+            case 'Update Employee Role':
+                return updateERole();
         }
     })
 };
@@ -34,38 +42,97 @@ const employeeOpt = () => {
 //         name: 'newDept',
 //         type: 'input',
 //         message: 'Enter new department name',
-//     }).then((answer) => {
+//     })
+//     .then((answer) => {
 //         connection.query(
-//             'INSERT INTO auctions SET ?',
-//             item_name: answer.item,//what the user typed in
+//             'INSERT INTO department SET ?',
+//         {
+//             department_name: answer.item,//what the user typed in
 //         },
 //         (err) => {
 //             if (err) throw err;
 //             console.log('Your auction was created successfully!');
-//     )}
-
-//     employeeOpt();
+//             employeeOpt(); 
+//         }
+//         );
+//     })
 // };
 
+// const addRole = () =>{
+//     inquirer
+//     .prompt([
+//         {
+//         name: 'newRole',
+//         type: 'input',
+//         message: 'Enter new role title',
+//         },
+//         {
+//         name: 'salary',
+//         type: 'input',
+//         message: 'Enter Salary',
+//         }
+//     ])
+//     .then((answer) => {
+//         connection.query(
+//             'INSERT INTO department SET ?',//
+//             {
+//             title: answer.item,
+//             salary: answer.item,
+//             },
+//             (err) => {
+//                 if (err) throw err;
+//                 console.log('Your auction was created successfully!');
+//                 employeeOpt();
+//             }
+//             );
+//     });
+// };
+// const addEmp = () =>{
+//     inquirer
+//     .prompt([
+//         {
+//         name: 'firstname',
+//         type: 'input',
+//         message: 'Enter employee first name',
+//         },
+//         {
+//         name: 'lastname',
+//         type: 'input',
+//         message: 'Enter employee last name',
+//         },
+//         {
+//         name: 'role',//
+//         type: 'input',
+//         message: 'roleid',//
+//         },
+//         {
+//         name: 'manid',
+//         type: 'input',
+//         message: 'managerid',//
+//         }
+//     ])
+//     .then((answer) => {
+//         connection.query(
+//             'INSERT INTO SET ?', //
+//             {
+//             first_name: answer.item,
+//             last_name: answer.item,
+//             role_id: answer.item,
+//             manager_id: answer.item
+//             },
+//             (err) => {
+//                 if (err) throw err;
+//                 console.log('Employee added successfully!');
+//                 employeeOpt();
+//             }
+//             );
+//     });
+// }
 
-function viewDept(){
-    console.log('This function works!');
-    employeeOpt();
-}
-
-function employeeUpdate(){
-    console.log('This function works!');
-    employeeOpt();
-}
-
-
-//use switch cases based on the options they choose it will send them to the func they choose 
-//
-
-//const addEmployee
-//inquirer prompt
-//.then answer and connection query  
-//insert into employee table
+// //const addEmployee
+// //inquirer prompt
+// //.then answer and connection query  
+// //insert into employee table
 
 
 employeeOpt();
